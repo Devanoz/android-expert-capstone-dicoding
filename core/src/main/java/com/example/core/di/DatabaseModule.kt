e5fb9aac2cfc0aa64b3eec4063fd8fa32a0680fe
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2023-09-08 21:36:17 +0700

    configure Sql chiper for room db

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,14 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): GamesDatabase =
-        Room.databaseBuilder(context, GamesDatabase::class.java,"Games.db")
-            .fallbackToDestructiveMigration().build()
-
+    fun provideDatabase(@ApplicationContext context: Context): GamesDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(context, GamesDatabase::class.java, "Games.db")
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
     @Provides
     fun providesGamesDao(database: GamesDatabase): GamesDao = database.gamesDao()
 }
